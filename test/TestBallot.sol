7d726798c0983d7a9efbd00c7b72a04da6e13a65
Arthur BONNECARRERE <abonneca@student.42.fr>
2017-10-31 12:04:55 +0100

    tests OK

--- test/TestBallot.sol
+++ test/TestBallot.sol
@@ -17,7 +17,7 @@ contract TestBallot {
 
   // Testing the UpVote() function
   function testUserUpVote() {
-    uint positiveVoteCount = 1;
+    int positiveVoteCount = 1;
     ballot.upVote(0);
     var (name, expected, neg) = (ballot.proposals(0));
     Assert.equal(positiveVoteCount, expected, "positiveVoteCount should equal 1");
@@ -26,9 +26,15 @@ contract TestBallot {
   // Testing the UpVote() function
   function testWinningContract() {
     ballot.addProposal("Second, loosing proposal");
-    ballot.downVote(0);
-    var (name, expected, neg) = (ballot.proposals(0));
-    Assert.equal(positiveVoteCount, expected, "positiveVoteCount should equal 1");
+    ballot.addProposal("Third, loosing proposal");
+    ballot.addProposal("Fourth, loosing proposal");
+    ballot.downVote(1);
+    ballot.downVote(2);
+    ballot.downVote(3);
+
+    var (expected, pos, neg) = ballot.proposals(0);
+    bytes32 winner = ballot.getWinnerName();
+    Assert.equal(winner, expected, "Winner should be \"My First Proposal\"");
   }
 
 }
