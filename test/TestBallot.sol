11e89164228997938718af66758bf992225d5657
Sydney COHEN <chnsydney@gmail.com>
2017-10-31 10:12:30 +0100

    "init"

---
+++ test/TestBallot.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.11;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Ballot.sol";
+
+contract TestBallot {
+  Ballot ballot = Ballot(DeployedAddresses.Ballot());
+
+
+    struct Voter {
+      uint weight;
+      mapping(uint => bool) votedProposal;
+    }
+
+  // Testing the hasVoted() function
+  function testaddProposal() {
+    bytes32 proposalName = "My Fisrt Proposal";
+    ballot.addProposal(proposalName);
+    var (expected, pos, neg) = (ballot.proposals(0));
+    Assert.equal(proposalName, expected, "ProposalName should match");
+  }
+
+    // Testing the hasVoted() function
+  function testUserUpVote() {
+    uint positiveVoteCount = 1;
+    ballot.upVote(0);
+    var (name, expected, neg) = (ballot.proposals(0));
+    Assert.equal(positiveVoteCount, expected, "positiveVoteCount should equal 1");
+  }
+}
